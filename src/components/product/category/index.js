e822cfbe5c09fe546ee8803af123c1fdf04a5c6a
Deepak <prajapathideepak4980@gmai.com>
2024-02-28 00:05:22 +0530

    offers

--- src/components/product/category/index.js
+++ src/components/product/category/index.js
@@ -6,6 +6,8 @@ import img3 from "../../../images/product/category/cat-3.jpg"
 import img4 from "../../../images/product/category/cat-4.jpg"
 import img5 from "../../../images/product/category/cat-5.jpg"
 import img6 from "../../../images/product/category/cat-6.jpg"
+import img7 from "../../../images/product/category/offer-1.png"
+import img8 from "../../../images/product/category/offer-2.png"
 
 const Category = () => {
     return (
@@ -83,7 +85,7 @@ const OfferComponent = () => {
             <div className="row px-xl-5">
                 <div className="col-md-6 pb-4">
                     <div className="position-relative bg-secondary text-center text-md-right text-white mb-2 py-5 px-5">
-                        <img src="img/offer-1.png" alt="" />
+                        <img src={img7} alt="" />
                         <div className="position-relative" style={{ zIndex: 1 }}>
                             <h5 className="text-uppercase text-primary mb-3">20% off the all order</h5>
                             <h1 className="mb-4 font-weight-semi-bold">Spring Collection</h1>
@@ -93,7 +95,7 @@ const OfferComponent = () => {
                 </div>
                 <div className="col-md-6 pb-4">
                     <div className="position-relative bg-secondary text-center text-md-left text-white mb-2 py-5 px-5">
-                        <img src="img/offer-2.png" alt="" />
+                        <img src={img8} alt="" />
                         <div className="position-relative" style={{ zIndex: 1 }}>
                             <h5 className="text-uppercase text-primary mb-3">20% off the all order</h5>
                             <h1 className="mb-4 font-weight-semi-bold">Winter Collection</h1>
